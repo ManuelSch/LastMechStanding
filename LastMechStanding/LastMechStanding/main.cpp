3d5e4c0029d76df45f7dc964ace7bd1adac1616c
Manuel <e1426298@student.tuwien.ac.at>
2017-04-07 23:39:19 +0200

    tutorial done: https://learnopengl.com/#!Getting-started/Hello-Window

---
+++ LastMechStanding/LastMechStanding/main.cpp
@@ -0,0 +1,80 @@
+// C++ libraries:
+#include <iostream>
+
+// OpenGL libraries
+#include <GL\glew.h>
+#include <GLFW\glfw3.h>
+
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+
+	// esc key -> close window:
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+}
+
+
+int main()
+{
+
+	// initialize glfw:
+	glfwInit();
+
+	// define OpenGL version (v3.3)
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	// use core profile only:
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// make window non resizable:
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	// create new window:
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// initialize glew:
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return -1;
+	}
+
+	// set viewport size:
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+
+	glViewport(0, 0, width, height);
+
+
+	// set key callback:
+	glfwSetKeyCallback(window, key_callback);
+
+	// game loop:
+	while (!glfwWindowShouldClose(window)) {
+
+		// check if any events were triggered:
+		glfwPollEvents();
+
+		// Clear color:
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// swap window and color buffer:
+		glfwSwapBuffers(window);
+	}
+
+	// clean up resources:
+	glfwTerminate();
+
+	return 0;
+}
