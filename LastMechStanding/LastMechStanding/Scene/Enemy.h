88505ff92e963e5cc5bbc3574ca7f282c1de8211
Michael Tischler <e1326028@student.tuwien.ac.at>
2017-04-27 09:32:38 +0200

    added enemy class

---
+++ LastMechStanding/LastMechStanding/Scene/Enemy.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "..\commonHeader.h"
+#include "SceneObject.h"
+
+#ifdef SCENE_OBJECT
+
+class Enemy : public SceneObject
+{
+public:
+	Enemy();
+	virtual ~Enemy();
+
+	virtual void update(float deltaTime);
+	virtual void draw(glm::mat4* viewMatrix, glm::mat4* projectionMatrix, Camera* camera, vector<shared_ptr<LightSource>>* lightSources);
+};
+
+#endif
