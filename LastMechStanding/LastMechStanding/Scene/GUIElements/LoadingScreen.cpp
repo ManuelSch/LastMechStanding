4f1851013e8b1c93efea61477f75b3de911ffb0e
Manuel <e1426298@student.tuwien.ac.at>
2017-06-19 18:01:16 +0200

    bug fix

--- LastMechStanding/LastMechStanding/Scene/GUIElements/LoadingScreen.cpp
+++ LastMechStanding/LastMechStanding/Scene/GUIElements/LoadingScreen.cpp
@@ -16,7 +16,7 @@ void LoadingScreen::draw(GLuint percent, shared_ptr<Display> display)
 {
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClearColor(0.45f, 0.78f, 1.0f, 1.0f);
 
 	this->font->renderText("Loading: " + to_string(percent) + "\%", 10.0f, 10.0f, 0.6f, glm::vec3(1.0f, 1.0f, 1.0f));
 
